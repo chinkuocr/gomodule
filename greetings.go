6036362d6dc7a0d7ff30ccbdba43e2fbbf34b051
chinkuocr <chinkuocr@yahoo.co.jp>
2023-01-24 11:34:20 +0900

    changes for v2.1

--- greetings.go
+++ greetings.go
@@ -6,6 +6,6 @@ import "fmt"
 
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Hello!", name)
+    message := fmt.Sprintf("Hi, %v. Hello World!", name)
     return message
 }
